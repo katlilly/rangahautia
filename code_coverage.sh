ab1e7194fa81a2ef362ee5cdc6073840e2bde166
Kat <kat.lilly@gmail.com>
2019-05-13 23:00:40 +1200

    code coverage environment

--- code_coverage.sh
+++ code_coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-g++ -fprofile-arcs -ftest-coverage vbyte_compress.cpp search.cpp -lm
+g++ -std=c++11 -fprofile-arcs -ftest-coverage vbyte_compress.cpp search.cpp -lm
 ./a.out < testfiles/testsearch.txt
 gcov search.cpp
 
