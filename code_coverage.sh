2703542274672afdf2d92b25dd665cd8b3a3d92a
Kat Lilly (brisk) <kat.lilly@gmail.com>
2019-05-21 18:28:51 +1200

    fix code coverage script

--- code_coverage.sh
+++ code_coverage.sh
@@ -2,7 +2,10 @@
 
 
 g++ -std=c++11 -fprofile-arcs -ftest-coverage vbyte_compress.cpp search.cpp -lm
-./a.out < testfiles/testsearch.txt
-gcov search.cpp
+g++ -std=c++11 -fprofile-arcs -ftest-coverage vbyte_compress.cpp growablearray.cpp tokeniser.cpp indexer.cpp -o indexer
 
+./indexer testfiles/test.xml
+./a.out < testfiles/testsearch.txt
+
+gcov indexer.cpp vbyte_compress.cpp tokeniser.cpp growablearray.cpp search.cpp
 
